6efdaf10ec3c227b452c827676205a8cdb9e89c1
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-05 12:03:58 +0500

    chore: initialized db context class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseInMemoryDatabase("InMem");
+});
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44caabbdff447a06163ed99a7cb075994f692667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a9ad4f81416115b024375dbefeb9e7a846422b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
