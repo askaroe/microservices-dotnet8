dfd754e68ea847d28fc6348c1b8326031934cb72
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-06 21:53:11 +0500

    chore: created repository for platform model

--- obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2a9ad4f81416115b024375dbefeb9e7a846422b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efdaf10ec3c227b452c827676205a8cdb9e89c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
