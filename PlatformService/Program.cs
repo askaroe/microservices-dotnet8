7e4d49c2c61d911d78a35e29225921eb17cf44fe
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-13 20:21:43 +0500

    chore: moved the folder

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -13,20 +13,18 @@ builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 // app.UseHttpsRedirection();
 
-app.UseRouting();
 
 app.MapControllers();
 
