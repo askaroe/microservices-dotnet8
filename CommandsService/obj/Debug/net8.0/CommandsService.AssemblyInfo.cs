344ee621fd33c5094f426c15c40a29f3110ffd47
Askar Oralkhan <askar4ik123@gmail.com>
2024-04-18 13:51:08 +0500

    Deployed command service to K8S

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920fe97f0cc229987d9a421f120f127b89932095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ac84d591e297b40f37811a8acfea10eef95a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
